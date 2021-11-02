192111194159c4a03e4b48abd1ff4732a089e9f3
Adam Shaw <adam.s.shaw1989@gmail.com>
2021-11-02 13:22:08 -0400

    Initial Commit

---
+++ exercises/node-calculator/main.js
@@ -0,0 +1,39 @@
+const readline = require("readline-sync")
+
+
+const first = readline.questionFloat(["Please enter your first number. "])
+
+const second = readline.questionFloat(["Please enter your second number. "]);
+
+let operations = ['add', 'subtract', 'divide', 'multiply'],
+  choice = readline.keyInSelect(operations, 'Please enter the operation to perform?');
+
+if (operations[choice] == 'add'){
+    console.log ("The result is " + add(first, second));
+
+}       else if (operations[choice] == 'subtract'){
+                console.log("The result is " + subtract(first,second))
+
+        }       else if (operations[choice] == 'divide'){
+                        console.log("The result is " + divide(first,second))
+        
+                }       else if (operations[choice] == 'multiply'){
+                        console.log ("The result is " + multiply(first, second))
+                        }
+
+function add (num1, num2){
+    return  (num1) + (num2);
+}
+
+function subtract (num1, num2){
+    return (num1) - (num2);
+}
+
+function divide (num1, num2){
+    return (num1) / (num2);
+}
+
+function multiply (num1, num2){
+    return (num1) * (num2);
+}
+
